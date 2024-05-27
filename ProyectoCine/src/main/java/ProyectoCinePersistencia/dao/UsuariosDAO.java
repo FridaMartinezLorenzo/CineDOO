bc861e585a1a821794db28244aab2fd60c583c2c
JoseRJ2 <147292005+JoseRJ2@users.noreply.github.com>
2024-05-26 23:39:21 -0600

    Realize la parte de usuarios

---
+++ ProyectoCine/src/main/java/ProyectoCinePersistencia/dao/UsuariosDAO.java
@@ -0,0 +1,23 @@
+package ProyectoCinePersistencia.dao;
+
+
+import ProyectoCinePersistencia.entities.Usuario;
+import java.util.List;
+
+public class UsuariosDAO {
+    public boolean Crear(Usuario usuario){
+        return false;
+    }
+    public boolean Actualizar(Usuario usuario){
+        return false;
+    }
+    public boolean Eliminar(int idUsuario){
+        return false;
+    }
+    public List<Usuario> Listar(){
+        return null;
+    }
+    public Usuario Buscar(int idUsuario){
+        return null;
+    }
+}

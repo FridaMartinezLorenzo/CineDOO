04da3cf85e79478f9075af10e7d095e652ce7c96
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2024-05-30 00:45:07 -0600

    Todo unido

--- ProyectoCine/src/main/java/Main.java
+++ ProyectoCine/src/main/java/Main.java
@@ -1,8 +1,11 @@
 
-import ProyectoCinePersistencia.dao.ganancia.GananciaDAOImpl;
-import ProyectoCinePersistencia.utils.MyBatisUtil;
-import ProyectoCinePersistencia.dao.login.LoginDAOImpl;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSessionFactory;
+
+import ProyectoCinePersistencia.dao.sala.SalaDAOImpl;
+import ProyectoCinePersistencia.entities.Sala;
+import ProyectoCinePersistencia.utils.MyBatisUtil;
 
 public class Main {
 
@@ -10,20 +13,43 @@ public class Main {
         // Crear instancia de SqlSessionFactory
         SqlSessionFactory sqlSessionFactory = MyBatisUtil.getSqlSessionFactory();
 
-// Crear instancia de GananciaDAO
-        GananciaDAOImpl gananciaDAO = new GananciaDAOImpl(sqlSessionFactory);
+        // Crear instancia de SalaDAOImpl
+        SalaDAOImpl salaDAO = new SalaDAOImpl(sqlSessionFactory);
 
-        // ID de la película para la cual se quieren obtener las ganancias
-        int idPelicula = 1; // Este valor puede ser cambiado según los datos disponibles en la base de datos
+        // Crear una nueva sala
+        Sala nuevaSala = new Sala();
+        nuevaSala.setNumAsiento(150);
 
-        // Obtener las ganancias de la película
-        Double ganancias = gananciaDAO.obtenerGanancias(idPelicula);
+        // Insertar la nueva sala en la base de datos
+        Sala salaCreada = salaDAO.Crear(nuevaSala);
+        System.out.println("Sala creada con ID: " + salaCreada.getIdSala());
 
-        // Mostrar las ganancias obtenidas
-if (ganancias != null) {
-            System.out.println("Ganancias de la película con ID " + idPelicula + ": " + ganancias);
+        // Buscar una sala por su ID
+        Sala salaEncontrada = salaDAO.Buscar(salaCreada.getIdSala());
+        if (salaEncontrada != null) {
+            System.out.println("Sala encontrada: ID = " + salaEncontrada.getIdSala() + ", NumAsiento = " + salaEncontrada.getNumAsiento());
         } else {
-            System.out.println("No se encontraron ganancias para la película con ID " + idPelicula);
+            System.out.println("Sala no encontrada.");
+        }
+
+        // Actualizar la sala
+        if (salaEncontrada != null) {
+            salaEncontrada.setNumAsiento(200);
+            salaDAO.Actualizar(salaEncontrada);
+            System.out.println("Sala actualizada: ID = " + salaEncontrada.getIdSala() + ", Nuevo NumAsiento = " + salaEncontrada.getNumAsiento());
+        }
+
+        // Listar todas las salas
+        List<Sala> listaSalas = salaDAO.Listar();
+        System.out.println("Lista de Salas:");
+        for (Sala sala : listaSalas) {
+            System.out.println("ID: " + sala.getIdSala() + ", NumAsiento: " + sala.getNumAsiento());
+        }
+
+        // Eliminar una sala
+        if (salaEncontrada != null) {
+            salaDAO.Eliminar(salaEncontrada.getIdSala());
+            System.out.println("Sala eliminada con ID: " + salaEncontrada.getIdSala());
         }
     }
 }
